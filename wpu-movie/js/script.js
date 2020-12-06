af2233390b895ef40a5ca7e994145e613da18332
Daffa Firdaus <71306814+BabyBlue-69@users.noreply.github.com>
2020-12-06 18:14:36 +0700

    benerin script nya
    sebelumnya error mulu scriptnya, ternyata banyak typo nya

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,85 +1,80 @@
-function searchMovie(){
-    $('#movie-list').html('');
-    $.ajax({
-        url:'http://www.omdbapi.com',
-        type:'get',
-        dataType:'json',
-        data: {
-            'apikey' : '1bfb6a90'
-            's' : $('#search-input').val()
-        },
-        success: function(result){
-            if(result.Response == "True"){
-                let movies = result.Search;
-                console.log(movies);
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                $.each(movies, function(i, data){
-                    $('#movie-list').append('
-                    <div class="col-md-4">
-                    <div class="card mb-3">
-                        <img src=`+ data.Poster +` class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">`+ data.Title +`</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
-                                <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+ data.imdbID +`">Lihat Detail</a>
-                            </div>
-                    </div>
-                    </div>
-                    `);
-                });
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-                $('#search-input').val('');
-                }else{
-                    $('#moview-list').html(`
-                    <div class="col">
-                        <h1 class="text-center">`+ result.Error +`</h1>
-                    </div>
-                    `);
-                }
-            }
-    });
-}
-
-$('#search-button').on('click', function(){
-    searchMovie();
-});
-
-$('#search-input').on('keyup', function(event){
-    if(event.keyCode == 13){
-        searchMovie();
-    }
-});
-
-$('#movie-list').on('click', '.see-detail', function(){
-    $.ajax({
-        url:'http://www.omdbapi.com',
-        type: 'get',
-        dataType: 'json',
-        data: {
-            'apikey' : '1bfb6a90',
-            'i' : $(this).data('id')
-        },
-        success: function(movie){
-            if( movie.Response == "True"){
-                $('.modal-body').html('
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src"`+ movies.Poster +`" class="img-fluid">
-                        </div>
-                        <div class="col-md-8">
-                            <ul class="col-md-8">
-                                <li class="list-group-item"><h3>`+ movies.Title +`</h3></li>
-                                <li class="list-group-item">Released : `+ movies.Released +`</li>
-                                <li class="list-group-item">Genre : `+ movies.Genre +`</li>
-                                <li class="list-group-item">Director : `+ movies.Director +`</li>
-                                <li class="list-group-item">Actors : `+ movies.Actors +`</li>
-                            </ul>
-                        </div>
+    <title>Fir Movie</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="navbar-brand" href="#">Fir Movie</a>
+                <button class="navbar-toogler" type="button" data-toggle="collapse" data-target="#navbarNavA1tMarkup" aria-controls="navbarNavA1Markup" aria-expanded="false" aria-label="Toggle Navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavA1tMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Cari Film</a>
                     </div>
                 </div>
-            ')
-            }
-        }
-    });
-});
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row mt-3 justify-content-center">
+            <div class="col-md-8">
+                <h1 class="text-center">Cari Film</h1>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Movie title" id="search-input">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-info" type="button" id="search-button">Cari</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row" id="movie-list">
+        </div>
+    </div>
+
+    <!-- Modal -->
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-tittle" id="exampleModalLabel">Cari Film</h5>
+                    <button type="button" class="close" data-dismis="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismis="modal">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <script src="js/script.js"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
